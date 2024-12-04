bf2cec5e3763c23f16ce99cf07255f87536e1531
Hotel Colonial <colonialiguacufoz@gmail.com>
2024-12-04 09:37:05 -0300

    header

--- src/components/ui/Home.tsx
+++ src/components/ui/Home.tsx
@@ -21,21 +21,30 @@ const HomePage = () => {
               Vai viajar e embarcar saindo do aeroporto internacional de Foz do
               Iguaçu?
             </p>
-            <h1 className="text-[3.5rem] md:text-[7rem] 2xl:text-[9rem] leading-[4rem] md:leading-[6rem] 2xl:leading-[7.2rem] text-green-950">
+            <h1 className="text-[3.5rem] md:text-[6rem] 2xl:text-[7rem] leading-[4rem] md:leading-[4.8rem] 2xl:leading-[5.5rem] text-green-950">
               ESTACIONE <br /> HOSPEDE-SE <br />
               VOE
             </h1>
-            <p className="text-xs md:text-base font-light">
+            <p className="text-xs md:text-base font-light py-2">
               Serviço exclusivo do{" "}
               <Link href="https://hotelcolonialfoz.com.br/">
-                <span className="text-green-950 font-nautical text-[2rem]">
+                <span className="text-gray-100 px-2 rounded-md py-1 bg-primary-green hover:bg-green-900 font-nautical text-[1.5rem] md:text-[2rem]">
                   Hotel Colonial Iguaçu
                 </span>
               </Link>
             </p>
+            <div>
+              <Image
+                src="/airport.jpg"
+                width={1400} // Tamaño máximo para imágenes grandes
+                height={900}
+                alt="parking"
+                className="object-contain sm:hidden rounded-xl py-3 opacity-70"
+              />
+            </div>
           </div>
 
-          <div className="absolute inset-0 flex justify-end items-center z-0">
+          <div className="hidden sm:flex absolute inset-0 justify-end items-center z-0">
             <div className="w-[1000px]">
               <Image
                 src="/airport.jpg"
