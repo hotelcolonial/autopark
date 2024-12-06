1d4d7bfb4e96da480c1e2ea2d6167fea2204c940
Hotel Colonial <colonialiguacufoz@gmail.com>
2024-12-06 16:33:10 -0300

    packages change

--- src/components/ui/Home.tsx
+++ src/components/ui/Home.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import Navbar from "./Navbar";
 import {
@@ -6,10 +8,79 @@ import {
   AiOutlineCustomerService,
   AiOutlineSafety,
 } from "react-icons/ai";
+
 import Link from "next/link";
 import { FaWhatsapp } from "react-icons/fa";
+import { useState } from "react";
+
+const packages = [
+  {
+    title: "Pacote até 4 dias",
+    description:
+      "Você pode utilizar a diária antes do embarque ou no retorno da sua viagem",
+    items: [
+      "Serviço de café no quarto para check-ou entre 03:00 a 05.:0 da manha",
+      "1 diária em apartamento Single ou Double em categoria Standard",
+      "Até 2 crianças com até 5 anos no mesmo quarto dos pais não pagam.",
+      "Traslado gratuito Hotel – Aeroporto – Hotel.",
+      "Não cobramos taxas adicionais, como ISS ou taxa de serviços.",
+    ],
+    subtitle: "1 Diária de Hotel + 4 Dias de Estacionamento",
+    borderColor: "border-purple-500",
+    price: "R$ 500,00",
+  },
+  {
+    title: "Pacote até 7 dias",
+    description:
+      "Você pode utilizar a diária antes do embarque ou no retorno da sua viagem",
+    items: [
+      "Serviço de café no quarto para check-ou entre 03:00 a 05:00 da manha",
+      "1 diária em apartamento Single ou Double em categoria Standard",
+      "Até 2 crianças com até 5 anos no mesmo quarto dos pais não pagam.",
+      "Traslado gratuito Hotel – Aeroporto – Hotel.",
+      "Não cobramos taxas adicionais, como ISS ou taxa de serviços.",
+    ],
+    subtitle: "1 Diária de Hotel + 7 Dias de Estacionamento",
+    borderColor: "border-teal-500",
+    price: "R$ 546,00",
+  },
+  {
+    title: "Pacote até 14 dias",
+    description:
+      "Você pode utilizar a diária antes do embarque ou no retorno da sua viagem",
+    items: [
+      "Serviço de café no quarto para check-ou entre 03:00 a 05:00 da manha",
+      "1 diária em apartamento Single ou Double em categoria Standard",
+      "Até 2 crianças com até 5 anos no mesmo quarto dos pais não pagam.",
+      "Traslado gratuito Hotel – Aeroporto – Hotel.",
+      "Não cobramos taxas adicionais, como ISS ou taxa de serviços.",
+    ],
+    borderColor: "border-yellow-500",
+    subtitle: "1 Diária de Hotel + 14 Dias de Estacionamento",
+    price: "R$ 630,00",
+  },
+];
 
 const HomePage = () => {
+  const [numPeople, setNumPeople] = useState(2);
+  const getAdjustedPrice = (pack) => {
+    if (numPeople === 2) return pack.price;
+
+    if (numPeople === 3) {
+      switch (pack.title) {
+        case "Pacote até 4 dias":
+          return "R$ 635,00";
+        case "Pacote até 7 dias":
+          return "R$ 681,00";
+        case "Pacote até 14 dias":
+          return "R$ 765,00";
+        default:
+          return pack.price;
+      }
+    }
+
+    return pack.price;
+  };
   return (
     <div className=" text-black overflow-hidden space-y-7">
       <div className="h-screen padding-container flex flex-col mb-6 overflow-hidden">
@@ -300,98 +371,80 @@ const HomePage = () => {
         id="packs"
         className="flex flex-col items-center py-10 mb-3 font-inter bg-green-950 bg-opacity-10"
       >
+        {/* Título y descripción */}
         <section className="text-center">
           <h2 className="text-3xl font-bold mb-2">Pacotes de Hospedagem</h2>
-          <p className="text-gray-600 mb-8">
+          <p className="text-gray-600 mb-4">
             Escolha o pacote que melhor se adapta à sua viagem e aproveite
             nossos benefícios exclusivos.
           </p>
+
+          {/* Switch para cambiar el número de personas */}
+          <div className="flex items-center justify-center gap-4 mb-8">
+            <button
+              className={`py-2 px-4 text-xs rounded-md ${
+                numPeople === 2 ? "bg-primary-green text-white" : "bg-gray-200"
+              }`}
+              onClick={() => setNumPeople(2)}
+            >
+              2 Pessoas
+            </button>
+            <button
+              className={`py-2 px-4 text-xs rounded-md ${
+                numPeople === 3 ? "bg-primary-green text-white" : "bg-gray-200"
+              }`}
+              onClick={() => setNumPeople(3)}
+            >
+              3 Pessoas
+            </button>
+          </div>
         </section>
 
+        {/* Paquetes */}
         <div className="flex flex-wrap justify-center gap-6">
-          {/* Pacote 4 dias */}
-          <div className="bg-white rounded-lg shadow-md border-t-4 border-purple-500 w-80 p-6">
-            <h2 className="text-xl font-semibold mb-2">Pacote 4 dias</h2>
-            <p className="text-gray-500 mb-4">
-              1 diária em apartamento Single ou Double para até 2 pessoas. Você
-              pode utilizar a diária antes do embarque ou no retorno da sua
-              viagem.
-            </p>
-            <ul className="text-gray-500 mb-4 list-disc pl-5">
-              <li>
-                Buffet de café da manhã incluso a partir das 05hrs da manhã.
-              </li>
-              <li>
-                Até 2 crianças com até 5 anos no mesmo quarto dos pais não
-                pagam.
-              </li>
-              <li>
-                Permanência gratuita de 4 dias de estacionamento para o seu
-                veículo.
-              </li>
-              <li>Traslado gratuito Hotel – Aeroporto – Hotel.</li>
-              <li>Apartamento na categoria Standard.</li>
-              <li>
-                Não cobramos taxas adicionais, como ISS ou taxa de serviços.
-              </li>
-            </ul>
-          </div>
+          {packages.map((pack, index) => (
+            <div
+              key={index}
+              className={`bg-white rounded-lg shadow-md border-t-4 ${pack.borderColor} w-[25rem] p-6`}
+            >
+              {/* Título y precio */}
+              <h2 className="text-lg font-semibold mb-2">{pack.title}</h2>
+              <p className="text-[2.5rem] xl:text-[3rem] font-black text-gray-700 my-4">
+                {getAdjustedPrice(pack)}{" "}
+                <span className="text-sm text-bold">
+                  / {numPeople} pessoa{numPeople > 1 ? "s" : ""}
+                </span>
+              </p>
 
-          {/* Pacote 7 dias */}
-          <div className="bg-white rounded-lg shadow-md border-t-4 border-teal-500 w-80 p-6">
-            <h2 className="text-xl font-semibold mb-2">Pacote 7 dias</h2>
-            <p className="text-gray-500 mb-4">
-              1 diária em apartamento Single ou Double para até 2 pessoas. Você
-              pode utilizar a diária antes do embarque ou no retorno da sua
-              viagem.
-            </p>
-            <ul className="text-gray-500 mb-4 list-disc pl-5">
-              <li>
-                Buffet de café da manhã incluso a partir das 05hrs da manhã.
-              </li>
-              <li>
-                Até 2 crianças com até 5 anos no mesmo quarto dos pais não
-                pagam.
-              </li>
-              <li>
-                Permanência gratuita de 4 dias de estacionamento para o seu
-                veículo.
-              </li>
-              <li>Traslado gratuito Hotel – Aeroporto – Hotel.</li>
-              <li>Apartamento na categoria Standard.</li>
-              <li>
-                Não cobramos taxas adicionais, como ISS ou taxa de serviços.
-              </li>
-            </ul>
-          </div>
+              {/* Subtítulo y descripción */}
+              <p className="text-sm text-gray-600 pb-4">{pack.subtitle}</p>
+              <p className="text-gray-800 italic mb-4 text-xs text-center">
+                {pack.description}
+              </p>
 
-          {/* Pacote 14 dias */}
-          <div className="bg-white rounded-lg shadow-md border-t-4 border-yellow-500 w-80 p-6">
-            <h2 className="text-xl font-semibold mb-2">Pacote 14 dias</h2>
-            <p className="text-gray-500 mb-4">
-              1 diária em apartamento Single ou Double para até 2 pessoas. Você
-              pode utilizar a diária antes do embarque ou no retorno da sua
-              viagem.
-            </p>
-            <ul className="text-gray-500 mb-4 list-disc pl-5">
-              <li>
-                Buffet de café da manhã incluso a partir das 05hrs da manhã.
-              </li>
-              <li>
-                Até 2 crianças com até 5 anos no mesmo quarto dos pais não
-                pagam.
-              </li>
-              <li>
-                Permanência gratuita de 4 dias de estacionamento para o seu
-                veículo.
-              </li>
-              <li>Traslado gratuito Hotel – Aeroporto – Hotel.</li>
-              <li>Apartamento na categoria Standard.</li>
-              <li>
-                Não cobramos taxas adicionais, como ISS ou taxa de serviços.
-              </li>
-            </ul>
-          </div>
+              {/* Lista de beneficios */}
+              <ul className="text-gray-500 mb-4 list-disc space-y-1 pl-5 text-sm">
+                {pack.items.map((item, itemIndex) => (
+                  <li key={itemIndex}>{item}</li>
+                ))}
+              </ul>
+
+              {/* Botón para WhatsApp */}
+              <Link
+                href={`https://wa.me/5508008191993?text=Tenho%20interesse%20no%20pacote:%20${encodeURIComponent(
+                  pack.title
+                )}%20-%20${encodeURIComponent(
+                  pack.subtitle
+                )}%20pelo%20valor%20de%20${encodeURIComponent(
+                  getAdjustedPrice(pack)
+                )}%20para%20${numPeople}%20pessoa${numPeople > 1 ? "s" : ""}`}
+                target="_blank"
+                className="ml-4 bg-primary-green font-medium text-gray-100 btn w-full md:w-auto"
+              >
+                Reserve aqui
+              </Link>
+            </div>
+          ))}
         </div>
       </section>
       <section id="location" className="padding-container">
