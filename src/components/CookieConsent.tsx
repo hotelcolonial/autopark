311bfcf36ab321aaff3a98ef971d954aee3052d4
Hotel Colonial <colonialiguacufoz@gmail.com>
2024-12-17 10:54:21 -0300

    banner button width

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -66,16 +66,16 @@ export const CookieConsent: React.FC = () => {
             </p>
           </div>
         </div>
-        <div className="flex items-center space-x-4 text-xs flex-col space-y-3 md:flex-row w-full sm:w-auto">
+        <div className="flex items-center space-x-4 text-xs flex-col space-y-3 sm:space-y-0 md:flex-row w-full sm:w-auto">
           <button
             onClick={handleReject}
-            className="px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300 transition-colors duration-300 w-full md:w-auto"
+            className="px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300 transition-colors duration-300 w-full md:w-28"
           >
             Rejeitar
           </button>
           <button
             onClick={handleAcceptAll}
-            className="px-4 py-2 bg-black text-white rounded hover:bg-gray-800 transition-colors duration-300 w-full md:w-auto"
+            className="px-4 py-2 bg-black text-white rounded hover:bg-gray-800 transition-colors duration-300 w-full md:w-auto w-30"
           >
             Aceitar Todos
           </button>
